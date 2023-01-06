ccb5cedfe8f5343aedba66b50907833caa6233c7
diegoram23 <diego.pta.oc@gmail.com>
2023-01-06 11:18:44 -0800

    test2

--- data.js
+++ data.js
@@ -1,28 +1,28 @@
 const characterData = {
     hero: {
         name: "Wizard",
-        avatar: "images/duende.png",
+        avatar: "./images/duende.png",
         health: 50,
         diceCount: 3,
         currentDiceScore: []
     },
     orc: {
         name: "jango",
-        avatar: "images/jango.png",
+        avatar: "./images/jango.png",
         health: 30,
         diceCount: 1,
         currentDiceScore: []
     },
     demon: {
         name: "Mas Poot",
-        avatar: "images/maspoot.png",
+        avatar: "./images/maspoot.png",
         health: 25,
         diceCount: 2,
         currentDiceScore: []
     },
     goblin: {
         name: "Duende",
-        avatar: "images/duende.png",
+        avatar: "./images/duende.png",
         health: 20,
         diceCount: 3,
         currentDiceScore: []
